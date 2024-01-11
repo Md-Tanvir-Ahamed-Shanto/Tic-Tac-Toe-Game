d2ec3393e38c0c0e8de7d91040ee761e7ba9bf42
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-01-11 21:53:45 +0600

    square

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-function App() {
+/*
+   Game
+    -> Board
+        -> Square
+    -> Hostory
+*/
+
+// Square Function ->
+
+function Square({value}){
   return (
-    <>
-    <h1>Hello Word</h1>
-    </>
-  )
+    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-xl">
+      {value}
+    </button>
+  );
 }
 
-export default App
+export default function Board() {
+  return (
+    <>
+      <div>
+        <Square value="X" />
+        <Square value="X" />
+        <Square value="X" />
+      </div>
+      <div>
+        <Square value="X" />
+        <Square value="X" />
+        <Square value="X" />
+      </div>
+      <div>
+        <Square value="X" />
+        <Square value="X" />
+        <Square value="X" />
+      </div>
+    </>
+  );
+}
+
+
